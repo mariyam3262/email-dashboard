e2a8f539a1d9a4b9c221d45d40ca0ab99555ea65
Mariyam Bloch <mariyambloch3262@gmail.com>
2022-07-17 23:39:59 +0530

    final commit

---
+++ emailsender/customer/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
+
+def email(request):
+	return render(request, 'customer/email.html')
+
+def emai_sent(request):
+	return render(request,'customer/email_sent.html')
+
+
+
+def email_info(request):
+	print(request.POST )
+	myto      = request.POST['to']
+	mysubject = request.POST['subject']
+	mymessage = request.POST['message']
+
+	# email
+	send_mail(mysubject,mymessage,settings.EMAIL_HOST_USER,[myto], fail_silently=False)
+	messages.success(request, 'Email Sent Successfully')
+	return redirect('/customer/email_sent/')
+
+
+
