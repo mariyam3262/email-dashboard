1dc2dd233df58c3f4c14a4179db12b0fbb7b1fa0
Mariyam Bloch <mariyam.bloch786@gmail.com>
2022-07-18 07:54:16 +0530

    final commit to new user

--- emailsender/customer/urls.py
+++ emailsender/customer/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from customer import views
 
+
+
 urlpatterns = [
     path('email/', views.email, name='email'),
     path('email_info/', views.email_info, name='email_info'),
