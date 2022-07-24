b453aa0f083dbc14159b2b71d9a03896c8a8d43e
Mariyam Bloch <mariyam.bloch786@gmail.com>
2022-07-24 18:00:56 +0530

    setting.py file  modification

--- emailsender/emailsender/settings.py
+++ emailsender/emailsender/settings.py
@@ -141,6 +141,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ecommercebeginner3262@gmail.com'
-EMAIL_HOST_PASSWORD = 'glwoppzhdmpzdcvd'
+EMAIL_HOST_PASSWORD = ''
 
 
